0a9423c90558b4b09001178b87b75e652c935e8e
Intan Kusuma R <61036923+ACD4869@users.noreply.github.com>
2020-11-01 02:58:48 +0700

    Add files via upload

---
+++ resources/admins/profile.blade.php
@@ -0,0 +1,124 @@
+@extends('layouts.master')
+
+@section('title')
+<title>Profile</title>
+@endsection
+
+@section('bar')
+<!-- HEADER MOBILE-->
+<header class="header-mobile d-block d-lg-none">
+    <div class="header-mobile__bar">
+        <div class="container-fluid">
+            <div class="header-mobile-inner">
+                <a class="logo" href="/">
+                    <h2>LIBRARY</h2>
+                </a>
+
+                <button class="hamburger hamburger--slider" type="button">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar-mobile">
+        <div class="container-fluid">
+            <ul class="navbar-mobile__list list-unstyled">
+                <li>
+                    <a href="/staff">
+                      <i class="fas fa-group"></i>Staff List</a>
+                </li>
+                <li>
+                    <a href="/admin-profile/{{auth()->user()->id}}">
+                      <i class="fas fa-user"></i>My Profile</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<!-- END HEADER MOBILE-->
+
+<!-- MENU SIDEBAR-->
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="/">
+            <h1>LIBRARY</h1>
+        </a>
+        <p><br />Area 51</p>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+              <li>
+                  <a href="/staff">
+                    <i class="fas fa-group"></i>Staff List</a>
+              </li>
+              <li>
+                  <a href="/admin-profile/{{auth()->user()->id}}">
+                    <i class="fas fa-user"></i>My Profile</a>
+              </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
+<!-- END MENU SIDEBAR-->
+@stop
+
+@section('content')
+<div class="main-content">
+  <div class="section__content section__content--p30">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-3">
+            <!-- Profile Image -->
+            <div class="card card-primary card-outline">
+              <div class="card-body box-profile">
+                <h3 class="profile-username text-center">{{$data_admin->name}}</h3>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col -->
+          <div class="col-md-9">
+            <div class="card">
+              <div class="card-header p-2">
+                <ul class="nav nav-pills">
+                  <li class="nav-item"><a class="nav-link active" href="#user" data-toggle="tab">User</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+                  <div class="tab-pane active" id="user">
+                    <form class="form-horizontal">
+                      <div class="form-group row">
+                        <label for="staticName" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                          <input type="text" readonly class="form-control" value="{{$data_admin->name}}">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                          <input type="text" readonly class="form-control" value="{{$data_admin->email}}">
+                        </div>
+                      </div>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.nav-tabs-custom -->
+          </div>
+          <!-- /.col -->
+        </div>
+    </div>
+  </div>
+</div>
+
+@endsection
